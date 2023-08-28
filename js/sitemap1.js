d0c35eb4576d4b46e3c164c507e99a173758e713
web1004 <gangsugi@gmail.com>
2023-08-28 10:53:37 +0900

    modify-23.8.28

--- js/sitemap1.js
+++ js/sitemap1.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
     if(site){
       $(this).find("span").html("<i class='fa fa-chevron-up'></i>");
-      $(".footer_sitemap").stop().css({"height":"240px","border-bottom":"1px solid #c3c3c3"});   
+      $(".footer_sitemap").stop().css({"height":"240px","border-bottom":"1px solid #c3c3c3"});
       $(".sitemap").fadeIn(); 
       site=false;
     }else{
