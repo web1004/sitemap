21781baceabc84cefb18bbc72f5d991fa6fc9229
web1004 <gangsugi@gmail.com>
2024-10-17 14:17:57 +0900

    수정-24.10.17

--- js/select2.js
+++ js/select2.js
@@ -4,33 +4,14 @@ $(document).ready(function(){
   $(".s1").click(function(){
     $(".m1").stop().slideToggle();
     $(this).toggleClass("active");
-
-    if(chk1){
-      $(this).find("span").html("<i class='fa fa-chevron-down'></i>");
-      $(this).find("h3").css({"opacity":"0"});
-      chk1=false;
-    }else{
-      $(this).find("span").html("<i class='fa fa-chevron-up'></i>");
-      $(this).find("h3").css({"opacity":"1"});
-      chk1=true;
-    }
+    $(this).find("span i").toggleClass("fa-chevron-up fa-chevron-down")
   });
 
   chk2 =true;
   $(".s2").click(function(){
     $(".m2").stop().slideToggle();
     $(this).toggleClass("active");
-
-    if(chk2){
-      $(this).find("span").html("<i class='fa fa-chevron-down'></i>");
-      $(this).find("h3").css({"opacity":"0"});
-      chk2=false;
-    }else{
-      $(this).find("span").html("<i class='fa fa-chevron-up'></i>");
-      $(this).find("h3").css({"opacity":"1"});
-      chk2=true;
-    }
+    $(this).find("span i").toggleClass("fa-chevron-up fa-chevron-down")
   });
-
 
 });
